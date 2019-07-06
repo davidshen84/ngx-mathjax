44b1549bdf8d8cdba03684a12e07b15877b04b6a
Xi Shen <davidshen84@gmail.com>
2019-07-06 11:45:00 +0930

    Fix CircleCI configuration file.

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -9,7 +9,10 @@ exports.config = {
     './src/**/*.e2e-spec.ts'
   ],
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'chrome',
+    'chromeOptions': {
+      'args': ['--no-sandbox', '--disable-gpu', 'window-size=1920,1080']
+    }
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
